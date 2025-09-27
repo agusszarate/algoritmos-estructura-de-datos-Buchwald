1b9c7f87b754d10cffb57469b03b7a5007d2aa15
Agustin Zarate <azarate@teknedatalabs.com>
2025-09-27 17:24:33 -0300

    Add interfaces for IteradorLista and Lista with method definitions

--- tdas/lista/lista.go
+++ tdas/lista/lista.go
@@ -1,1 +1,49 @@
 package lista
+
+type IteradorLista[T any] interface {
+
+	// HaySiguiente devuelve verdadero si hay más elementos por iterar, false en caso contrario.
+	HaySiguiente() bool
+
+	// VerActual obtiene el valor del elemento actual. Si no hay siguiente elemento, entra en pánico con un mensaje
+	// "El iterador termino de iterar".
+	VerActual() T
+
+	// Siguiente avanza al siguiente elemento y devuelve su valor. Si no hay siguiente elemento, entra en pánico con un mensaje
+	// "El iterador termino de iterar".
+	Siguiente() T
+}
+
+type Lista[T any] interface {
+
+	// EstaVacia devuelve verdadero si la lista no tiene elementos, false en caso contrario.
+	EstaVacia() bool
+
+	// InsertarPrimero agrega un nuevo elemento al principio de la lista.
+	InsertarPrimero(T)
+
+	// InsertarUltimo agrega un nuevo elemento al final de la lista.
+	InsertarUltimo(T)
+
+	// BorrarPrimero borra el primer elemento de la lista. Si la lista tiene elementos, se quita el primero de la misma,
+	// y se devuelve ese valor. Si está vacía, entra en pánico con un mensaje "La lista esta vacia".
+	BorrarPrimero() T
+
+	// VerPrimero obtiene el valor del primer elemento de la lista. Si está vacía, entra en pánico con un mensaje
+	// "La lista esta vacia".
+	VerPrimero() T
+
+	// VerUltimo obtiene el valor del último elemento de la lista. Si está vacía, entra en pánico con un mensaje
+	// "La lista esta vacia".
+	VerUltimo() T
+
+	// Largo devuelve la cantidad de elementos de la lista.
+	Largo() int
+
+	// Iterar itera la lista, aplicando la función visitar a cada elemento, hasta que ésta devuelva false o se terminen
+	// los elementos de la lista.
+	Iterar(visitar func(T) bool)
+
+	// Iterador devuelve un iterador para recorrer la lista.
+	Iterador() IteradorLista[T]
+}
