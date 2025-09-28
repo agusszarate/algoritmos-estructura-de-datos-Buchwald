f07fb25389b2a7c39c20906635ef35a3ab0318fe
Agustin Zarate <azarate@teknedatalabs.com>
2025-09-28 17:50:46 -0300

    Implement linked list structure and methods in lista_enlazada.go

--- tdas/lista/lista_enlazada.go
+++ tdas/lista/lista_enlazada.go
@@ -1,1 +1,39 @@
 package lista
+
+type nodoLista[T any] struct {
+	dato      T
+	siguiente *nodoLista[T]
+}
+type listaEnlazada[T any] struct {
+	primero *nodoLista[T]
+	ultimo  *nodoLista[T]
+	largo   int
+}
+
+func (lista listaEnlazada[T]) EstaVacia() bool {
+	return lista.largo == 0 && lista.primero == nil && lista.ultimo == nil
+}
+
+func (lista *listaEnlazada[T]) InsertarPrimero(elemeneto T) {
+
+}
+
+func (lista *listaEnlazada[T]) InsertarUltimo(elemento T) {
+
+}
+
+func (lista *listaEnlazada[T]) BorrarPrimero() T {
+
+}
+
+func (lista listaEnlazada[T]) VerPrimero() T {
+
+}
+
+func (lista listaEnlazada[T]) VerUltimo() T {
+
+}
+
+func (lista listaEnlazada[T]) Largo() int {
+	return lista.largo
+}
