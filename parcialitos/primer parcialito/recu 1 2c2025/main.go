1f4754e68a2fd530efb094883f444a88ad7c869a
Agustin Zarate <azarate@teknedatalabs.com>
2025-11-20 21:50:02 -0300

    mi parcial

--- parcialitos/primer parcialito/recu 1 2c2025/main.go
+++ parcialitos/primer parcialito/recu 1 2c2025/main.go
@@ -13,10 +13,22 @@ import (
 // muchísimo más grande que n).
 //
 // Implementar un algoritmo que ordene las cadenas por dicho criterio en O(n).
-// Justificar brevemente por qué es correcta la aplicación del algoritmo que
-// implementaste. Justificar la complejidad del algoritmo implementado.
+// La firma de la función debe ser:
+// func Ordenar(cadenas []string, valoresHash []int64, K int64) []string
+//
+// valoresHash ya tiene cada valor posterior a haber hecho % K.
+// Recomendamos recordar las propiedades de las funciones de hashing.
+// Si necesitás un algoritmo de ordenamiento auxiliar al que estés implementando,
+// podés considerarlo ya implementado.
+// Justificar brevemente por qué es correcta la aplicación del algoritmo que implementaste.
+// Justificar la complejidad del algoritmo implementado.
+//
+// Ejemplo: si queremos ordenar las cadenas: gato, perro, elefante, comadreja
+// y los resultados de aplicarles la función de hashing (y % K) son 19, 703, 9872, 37,
+// respectivamente, las cadenas deben de quedar: gato, comadreja, perro, elefante.
+
 func Ordenar(cadenas []string, valoresHash []int64, K int64) []string {
-	// TODO: Implementar aquí
+	// TODO: Implementar solución
 	return nil
 }
 
@@ -24,10 +36,10 @@ func Ordenar(cadenas []string, valoresHash []int64, K int64) []string {
 // EJERCICIO 2
 // =============================================================================
 // Implementar un algoritmo que reciba un arreglo de enteros desordenado y un
-// número elem que, por división y conquista determine si elem se encuentra
-// en el arreglo.
-//
+// número elem que, por división y conquista determine si elem se encuentra en
+// el arreglo.
 // Indicar y justificar adecuadamente la complejidad del algoritmo implementado.
+
 func existe(arr []int, elem int) bool {
 	if len(arr) == 0 {
 		return false
@@ -48,19 +60,17 @@ func existe(arr []int, elem int) bool {
 	}
 }
 
-// T(o) = 2 * T(n/2) + O(n^0)
-// log2 2 = 1 > 0
-// complejidad: O(n)
-
 // =============================================================================
 // EJERCICIO 3
 // =============================================================================
 // Implementar una función que reciba una pila de enteros y devuelva la suma de
 // todos los elementos. Al finalizar la ejecución de la función, la pila debe
 // quedar en el mismo estado que tenía antes de ejecutar la misma.
-//
 // La función no puede utilizar estructuras auxiliares (incluyendo arreglos).
+// La firma de la función debe ser pilaSumar(pila Pila[int]) int.
 // Indicar y justificar la complejidad de la función implementada.
+
+// Definición de la interfaz necesaria para la firma de la función
 
 func pilaSumar(pila Pila[int]) int {
 
